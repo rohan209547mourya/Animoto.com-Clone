70e8467fca173a0798afe08d75f53aabb8547904
GAURAV SUDHANSHU <gauravsud3@gmail.com>
2022-07-20 12:08:15 +0530

    first

---
+++ Header/header.js
@@ -0,0 +1,5 @@
+let x =()=> {
+    return ` `;
+
+}
+export default x;
---
+++ index.js
@@ -0,0 +1,5 @@
+// import x from "./Header/header.js";
+// console.log(x())
+// document.getElementById("header").innerHTML = x();
+import footer from "./footer/footer.js";
+document.getElementById("footer").innerHTML=footer();
