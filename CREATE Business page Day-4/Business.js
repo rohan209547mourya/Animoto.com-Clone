9815ce0b8b42d2b3c187a9b05af10144bff5e24a
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2022-07-22 16:09:21 +0530

    CREATE=> Business Page

---
+++ CREATE Business page Day-4/Business.js
@@ -0,0 +1,173 @@
+let Opimg1 = document.getElementById("Opimg1");
+let Opimg2 = document.getElementById("Opimg2");
+let Opimg3 = document.getElementById("Opimg3");
+
+let OpH1 = document.getElementById("OpH1");
+let OpH2 = document.getElementById("OpH2");
+let OpH3 = document.getElementById("OpH3");
+
+let Op1 = document.getElementById("Op1");
+let Op2 = document.getElementById("Op2");
+let Op3 = document.getElementById("Op3");
+let Op4 = document.getElementById("Op4");
+let Op5 = document.getElementById("Op5");
+
+
+
+
+function Opt1(){
+
+  Opimg1.src="https://images.ctfassets.net/00i767ygo3tc/6GUMhhRJR9hQVm8o5062cV/6b0721f80cd4512ceeb653a6f5a09e32/video_ideas_for_business_desktop.jpg?w=525&h=295&q=50&fm=webp";
+  Opimg2.src="https://images.ctfassets.net/00i767ygo3tc/k29C7E9EnJQoDVj2Zxh8o/13e0d6de960d9e199e4ff454d2657903/property_listing_video_template_cover_image.jpg?w=525&h=295&q=50&fm=webp";
+  Opimg3.src="https://images.ctfassets.net/00i767ygo3tc/4USwhUH1gBg5Bvf6RXyTN4/889c671208000794780ebf2e75e31488/video_template_for_real_estate_agent_thumbnail.jpg?w=525&h=295&q=50&fm=webp";
+
+
+  OpH1.innerText="REAL ESTATE EXPLAINER";
+  OpH2.innerText="PROPERTY LISTING";
+  OpH3.innerText="AGENT TESTIMONIAL";
+
+  Op1.style.textDecoration="underline";
+  Op2.style.textDecoration="none";
+  Op3.style.textDecoration="none";
+  Op4.style.textDecoration="none";
+  Op5.style.textDecoration="none";
+
+}
+
+
+function Opt2(){
+
+  Opimg1.src="https://images.ctfassets.net/00i767ygo3tc/aLDufWrWsfLsQ98AgoI9f/a9653290bcb6285bac99b1068d383445/Personal_trainer_video_for_social_media.jpg?w=525&h=295&q=50&fm=webp";
+  Opimg2.src="https://images.ctfassets.net/00i767ygo3tc/57oFwPwuyKCFXqq7JvfjNL/b0542a85f9e76932cefebe7ead335de4/make_a_video_for_fitness_studio.jpg?w=525&h=295&q=50&fm=webp";
+  Opimg3.src="https://images.ctfassets.net/00i767ygo3tc/4iGwcjc7V72aWifXlUinhm/7adfd9d129fcc7f3627537adc440a830/Follow_along_workout_studio_video.jpg?w=525&h=295&q=50&fm=webp";
+
+
+  OpH1.innerText="COMPANY OVERVIEW";
+  OpH2.innerText="ONLINE FITNESS PROMO";
+  OpH3.innerText="FOLLOW-ALONG WORKOUT";
+
+  Op1.style.textDecoration="none";
+  Op2.style.textDecoration="underline";
+  Op3.style.textDecoration="none";
+  Op4.style.textDecoration="none";
+  Op5.style.textDecoration="none";
+
+}
+
+
+
+function Opt3(){
+
+  Opimg1.src="https://images.ctfassets.net/00i767ygo3tc/5V4wi8CRfHAgUylsxsstOz/530424f67ef1570489d1357101a075c9/New_Launch_Promo_video_diy.jpg?w=525&h=295&q=50&fm=webp";
+  Opimg2.src="https://images.ctfassets.net/00i767ygo3tc/5Xt8uuWf9rPUFU0anpGjhS/06e1c7d52dfbe91c1770ef341d735501/make_product_video_template.jpg?w=525&h=295&q=50&fm=webp";
+  Opimg3.src="https://images.ctfassets.net/00i767ygo3tc/415Vmb6UDnd46ySuOOMREl/b9e582a87935bba708b94ea1979c33f4/bite_sized_intro.jpg?w=525&h=295&q=50&fm=webp";
+
+
+  OpH1.innerText="NEW LAUNCH PROMO";
+  OpH2.innerText="COLLECTION PORTFOLIO";
+  OpH3.innerText="BITE-SIZED PRODUCT INTRO";
+
+  Op1.style.textDecoration="none";
+  Op2.style.textDecoration="none";
+  Op3.style.textDecoration="underline";
+  Op4.style.textDecoration="none";
+  Op5.style.textDecoration="none";
+
+}
+
+
+function Opt4(){
+
+  Opimg1.src="https://images.ctfassets.net/00i767ygo3tc/1ivgEHbIgS5OVhQiik0eup/08ca0e722cc1dbfb763e6c6a64eab621/photographer_behind_the_scenes_video_template.jpg?w=525&h=295&q=50&fm=webp";
+  Opimg2.src="https://images.ctfassets.net/00i767ygo3tc/7IEWORfYtfdewdLVrosnCd/ea862f02fb4d46379f808d1d07ba6d55/digital_album_video_photography_template.jpg?w=525&h=295&q=50&fm=webp";
+  Opimg3.src="https://images.ctfassets.net/00i767ygo3tc/7bDGGrOwRsJepekgK2ELY9/fb03498b5a7e74d4e5b5646456a53691/Inspirational_Social_Ad.jpg?w=525&h=295&q=50&fm=webp";
+
+
+  OpH1.innerText="BEHIND THE SCENES";
+  OpH2.innerText="DIGITAL ALBUM";
+  OpH3.innerText="INSPIRATIONAL SOCIAL AD";
+
+  Op1.style.textDecoration="none";
+  Op2.style.textDecoration="none";
+  Op3.style.textDecoration="none";
+  Op4.style.textDecoration="underline";
+  Op5.style.textDecoration="none";
+
+}
+
+function Opt5(){
+
+  Opimg1.src="https://images.ctfassets.net/00i767ygo3tc/5dFdbWxCaUFEvXCnYwmPm/3d2059bbda43154ecf70d96d13b12d9a/make_a_video_for_nonprofit_with_templates.jpg?w=234&h=234&q=50&fm=webp";
+  Opimg2.src="https://images.ctfassets.net/00i767ygo3tc/2ant08XkXw80BVYSJ0QLbA/5e33cefa016414882f188956b14fcaba/nonprofit_video_ad_fundraising_explainer_template.jpg?w=234&h=234&q=50&fm=webp";
+  Opimg3.src="https://images.ctfassets.net/00i767ygo3tc/7MsRKKVyZbJ3vXU4Aj1qXO/f2b8c029ebcff130ad9e5fbe19be1011/charity_video_for_nonprofits.jpg?w=234&h=234&q=50&fm=webp";
+
+
+  OpH1.innerText="SOCIAL FUNDRAISING AD";
+  OpH2.innerText="FUNDRAISING EXPLAINER";
+  OpH3.innerText="WEEKLY FEATURE";
+
+  Op1.style.textDecoration="none";
+  Op2.style.textDecoration="none";
+  Op3.style.textDecoration="none";
+  Op4.style.textDecoration="none";
+  Op5.style.textDecoration="underline";
+
+}
+
+
+
+//--------------------------------faqs--------------------------------------------------------------------
+
+let para1 = document.getElementById("para1");
+let para2 = document.getElementById("para2");
+let para3 = document.getElementById("para3");
+
+let Plus1 = document.getElementById("Plus1");
+
+function FAQ1() {
+  if (Plus1.innerText == "x") {
+    para1.innerText = "";
+    Plus1.innerText = "+";
+  } else if (Plus1.innerText == "+") {
+    para1.innerText =
+      "Social channels are prioritizing video content over all others, and for good reason. Marketing videos not only boost conversions and sales by as much as 80%, they can build trust, drive engagement, and increase brand recall, and are more likely to be shared than static images. Ready to start building your video marketing strategy? Try out Animoto's Product Promotion templates.";
+    para2.innerText = "";
+    para3.innerText = "";
+
+    Plus1.innerText = "x";
+    Plus2.innerText = "+";
+    Plus3.innerText = "+";
+  }
+}
+
+function FAQ2() {
+  if (Plus2.innerText == "x") {
+    para2.innerText = "";
+    Plus2.innerText = "+";
+  } else if (Plus2.innerText == "+") {
+    para1.innerText = "";
+    para2.innerText =
+      "When starting from scratch of a marketing video, you have to outline the shots, write a script, hire talent, set aside time to film, edit the video, and more. When you use a business video maker like Animoto, all you have to do is drag and drop photo and video clips into premade templates and edit the text to create a professional video promo in minutes. Try it out for free!";
+    para3.innerText = "";
+
+    Plus1.innerText = "+";
+    Plus2.innerText = "x";
+    Plus3.innerText = "+";
+  }
+}
+
+function FAQ3() {
+  if (Plus3.innerText == "x") {
+    para3.innerText = "";
+    Plus3.innerText = "+";
+  } else if (Plus3.innerText == "+") {
+    para1.innerText = "";
+    para2.innerText = "";
+    para3.innerText =
+      "Nearly every social channel is prioritizing video content over all others. Today, marketing videos and ads can be shared on social channels like Facebook, Youtube, Twitter, Instagram, and TikTok and added to your website, pitch decks, and more. Start creating professional videos for free with Animoto!";
+
+    Plus1.innerText = "+";
+    Plus2.innerText = "+";
+    Plus3.innerText = "x";
+  }
+}
