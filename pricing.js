d312618ce73bfd41ead0956263d2134a9563ef7e
anmol7895 <anmolpandit1023@gmail.com>
2022-07-23 17:40:45 +0530

    day-5 done

--- pricing.js
+++ pricing.js
@@ -16,6 +16,22 @@ document.getElementById("menuBtn").addEventListener("click", () => {
 
 
 
+// const toggler = document.querySelector(".toggler");
+
+// const starterPrice = document.getElementById("starter-price");
+// const proPrice = document.getElementById("pro-price");
+
+// toggler.addEventListener("change", ()=>{
+//     if(toggler.checked){
+//         starterPrice.innerHTML = `$200 <span> / year </span>`;
+//         proPrice.innerHTML = `$1000 <span> / year </span>`;
+//     }else{
+//         starterPrice.innerHTML = `$19 <span> / month </span>`;
+//         proPrice.innerHTML = `$99 <span> / month </span>`; 
+//     }
+// })
+
+
 const toggler = document.querySelector(".toggler");
 const starterPrice = document.getElementById("starter-price");
 const proPrice = document.getElementById("pro-price");
